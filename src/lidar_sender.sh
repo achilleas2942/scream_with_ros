b3539714c8eb10df69e310c90a2225e470099ebd
Achilleas Seisa <achilleas2942@gmail.com>
2025-02-25 23:58:58 +0100

    Fix bugs

--- src/lidar_sender.sh
+++ src/lidar_sender.sh
@@ -7,23 +7,26 @@ INIT_ENC_BITRATE=5000
 
 if (($USE_SCREAM == 1)); then
     INIT_ENC_BITRATE=500
-    #NOSUMMARY=" -nosummary"
     
-    SCREAMTX0="queue ! screamtx name=\"screamtx0\" params=\"$NOSUMMARY -forceidr $SCREAMTX_PARAM_ECT -initrate $INIT_ENC_BITRATE  -minrate 200 -maxrate 8000\" ! queue !"
+    SCREAMTX0="queue ! screamtx name=\"screamtx0\" params=\" -forceidr -initrate $INIT_ENC_BITRATE -minrate 200 -maxrate 8000\" ! queue !"
     SCREAMTX0_RTCP="screamtx0.rtcp_sink screamtx0.rtcp_src !"
 else
     SCREAMTX0=""
     SCREAMTX0_RTCP=""
 fi
 
-# Update for LiDAR streaming (ROS 1 or ROS 2 should match accordingly)
-LIDARSRC="udpsrc port=4000 caps=\"application/x-raw,media=lidar\" ! queue ! "
+# Updated for LiDAR streaming
+LIDARSRC="udpsrc port=4000 caps=\"application/x-raw,media=lidar\" ! queue !"
 
 export SENDPIPELINE="rtpbin name=r \
 $LIDARSRC $SCREAMTX0 r.send_rtp_sink_0 r.send_rtp_src_0 ! udpsink host=$RECEIVER_IP port=$PORT0_RTP sync=false $SET_ECN \
-   udpsrc port=$PORT0_RTCP address=$SENDER_IP ! queue ! $SCREAMTX0_RTCP r.recv_rtcp_sink_0 \
+   udpsrc port=$PORT0_RTCP address=127.0.0.1 ! queue ! $SCREAMTX0_RTCP r.recv_rtcp_sink_0 \
    r.send_rtcp_src_0 ! udpsink host=$RECEIVER_IP port=$PORT0_RTCP sync=false async=false \
 "
+
 export GST_DEBUG="2,screamtx:2"
-killall -9 scream_sender
+
+# Use pkill instead of killall for better compatibility
+pkill -9 scream_sender
+
 $SCREAM_TARGET_DIR/scream_sender --verbose
