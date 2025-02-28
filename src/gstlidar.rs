a71a5143592bc9a7e3d6c3084032121e477e8d29
Achilleas Seisa <achilleas2942@gmail.com>
2025-02-28 11:03:49 +0100

    Fix bugs

--- src/gstlidar.rs
+++ src/gstlidar.rs
@@ -1,12 +1,12 @@
 use gst::glib;
-use gst_base::subclass::base_transform::{BaseTransform, BaseTransformMode};
-use gst_base::subclass::prelude::*;  
-use gst::subclass::prelude::*;        
+use gst_base::BaseTransform;
+use gst_base::subclass::prelude::*;
+use gst::subclass::prelude::*;
 use gst::prelude::*;
 
 glib::wrapper! {
     pub struct LidarTransform(ObjectSubclass<imp::LidarTransform>) 
-        @extends gst_base::BaseTransform, gst::Element, gst::Object;
+        @extends BaseTransform, gst::Element, gst::Object;
 }
 
 mod imp {
@@ -19,14 +19,20 @@ mod imp {
     #[glib::object_subclass]
     impl ObjectSubclass for LidarTransform {
         const NAME: &'static str = "LidarTransform";
+        type Type = super::LidarTransform;
         type ParentType = BaseTransform;
     }
 
     impl ObjectImpl for LidarTransform {}
+    impl GstObjectImpl for LidarTransform {}  // ✅ Add this
     impl ElementImpl for LidarTransform {}
-    impl BaseTransformImpl for LidarTransform {}
+    impl BaseTransformImpl for LidarTransform {
+        const MODE: BaseTransformMode = BaseTransformMode::AlwaysInPlace;
+        const PASSTHROUGH_ON_SAME_CAPS: bool = false;
+        const TRANSFORM_IP_ON_PASSTHROUGH: bool = false;
+    }
 }
 
 pub fn register(plugin: &gst::Plugin) -> Result<(), glib::BoolError> {
-    LidarTransform::register(plugin)
+    gst::Element::register(Some(plugin), "lidartransform", gst::Rank::None, LidarTransform::static_type())
 }
