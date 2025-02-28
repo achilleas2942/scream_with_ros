160a2304e1ca52b995ffc69bce39918640c97472
Achilleas Seisa <achilleas2942@gmail.com>
2025-02-28 10:58:58 +0100

    Fix bugs

--- src/gstlidar.rs
+++ src/gstlidar.rs
@@ -1,91 +1,32 @@
 use gst::glib;
-use gst::subclass::prelude::*;
+use gst_base::subclass::base_transform::{BaseTransform, BaseTransformMode};
+use gst_base::subclass::prelude::*;  
+use gst::subclass::prelude::*;        
 use gst::prelude::*;
-use gst_base::subclass::BaseTransform;
-use gst_base::BaseTransformMode;
-use once_cell::sync::Lazy;
-use std::sync::Mutex;
 
-// Logging Category
-static CAT: Lazy<gst::DebugCategory> = Lazy::new(|| {
-    gst::DebugCategory::new(
-        "lidar",
-        gst::DebugColorFlags::empty(),
-        Some("LiDAR Data Transformer"),
-    )
-});
-
-// Element Metadata
-#[derive(Default)]
-pub struct LidarTransform {
-    settings: Mutex<LidarSettings>,
+glib::wrapper! {
+    pub struct LidarTransform(ObjectSubclass<imp::LidarTransform>) 
+        @extends gst_base::BaseTransform, gst::Element, gst::Object;
 }
 
-#[derive(Default)]
-struct LidarSettings {
-    // Placeholder for settings like filtering LiDAR points
-}
+mod imp {
+    use super::*;
+    use gst_base::subclass::base_transform::BaseTransformImpl;
 
-#[glib::object_subclass]
-impl ObjectSubclass for LidarTransform {
-    const NAME: &'static str = "GstLidarTransform";
-    type Type = super::LidarTransform;
-    type ParentType = gst_base::BaseTransform;
-}
+    #[derive(Default)]
+    pub struct LidarTransform;
 
-// GObject Registration
-impl ObjectImpl for LidarTransform {}
-
-impl GstObjectImpl for LidarTransform {}
-
-impl ElementImpl for LidarTransform {
-    fn metadata() -> Option<&'static gst::subclass::ElementMetadata> {
-        Some(gst::subclass::ElementMetadata::new(
-            "LiDAR Transformer",
-            "Filter",
-            "Processes LiDAR data in a GStreamer pipeline",
-            "Your Name <your@email.com>",
-        ))
+    #[glib::object_subclass]
+    impl ObjectSubclass for LidarTransform {
+        const NAME: &'static str = "LidarTransform";
+        type ParentType = BaseTransform;
     }
 
-    fn pad_templates() -> &'static [gst::PadTemplate] {
-        static PAD_TEMPLATES: Lazy<Vec<gst::PadTemplate>> = Lazy::new(|| {
-            let caps = gst::Caps::new_simple("application/x-raw", &[("media", &"lidar")]);
-            vec![
-                gst::PadTemplate::new(
-                    "sink",
-                    gst::PadDirection::Sink,
-                    gst::PadPresence::Always,
-                    &caps,
-                )
-                .unwrap(),
-                gst::PadTemplate::new(
-                    "src",
-                    gst::PadDirection::Src,
-                    gst::PadPresence::Always,
-                    &caps,
-                )
-                .unwrap(),
-            ]
-        });
-        PAD_TEMPLATES.as_ref()
-    }
+    impl ObjectImpl for LidarTransform {}
+    impl ElementImpl for LidarTransform {}
+    impl BaseTransformImpl for LidarTransform {}
 }
 
 pub fn register(plugin: &gst::Plugin) -> Result<(), glib::BoolError> {
     LidarTransform::register(plugin)
-}
-
-// BaseTransform Implementation (Data Processing)
-impl BaseTransformImpl for LidarTransform {
-    const MODE: BaseTransformMode = BaseTransformMode::Passthrough;
-
-    fn transform_ip(
-        &self,
-        _element: &Self::Type,
-        buffer: &mut gst::Buffer,
-    ) -> Result<gst::FlowSuccess, gst::FlowError> {
-        gst::debug!(CAT, "Processing LiDAR Buffer {:?}", buffer);
-        Ok(gst::FlowSuccess::Ok)
-    }
 }
