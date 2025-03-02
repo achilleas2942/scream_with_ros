c6746859293f28d2fe2a0442b7c4c6d33cb45014
Achilleas Seisa <achilleas2942@gmail.com>
2025-03-02 14:13:04 +0100

    Change image tag name

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -2,4 +2,4 @@ echo "--------------------------------------"
 echo " building scream with ros docker image"
 echo "--------------------------------------"
 
-DOCKER_BUILDKIT=1 docker build -t ghcr.io/achilleas2942/scream-with-ros:latest .
+DOCKER_BUILDKIT=1 docker build -t ghcr.io/achilleas2942/scream-with-ros:lidar2image .
